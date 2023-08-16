51c8a8c23c0b18ba5a122d2ba148fae6356de0f0
imSaikirann <neelakantamsaikiran7@gmail.com>
2023-08-16 18:53:05 +0530

    first commit

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ require('dotenv').config();
 const dFrontend = require('./routes/detailedFRoute');
 const UserLoginDetails = require('./routes/userRoutes');
 const checkboxUpdate = require('./routes/checkboxRoute');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 
 app.use(express.json());
 
-// Use the cors middleware
 app.use(cors({
   origin: 'http://localhost:3000', 
   credentials: true, 
@@ -19,23 +18,20 @@ app.use('/api/detailedFrontend', dFrontend);
 app.use('/api/user', UserLoginDetails);
 app.use('/api/updateCheckbox', checkboxUpdate);
 
-// Middleware
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();  
 });
 
-// New Hello, World! route
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// Connection to the database
-mongoose.connect(process.env.MONG) // Correct the process.env variable
+mongoose.connect(process.env.MONGO_URI) // Corrected the environment variable name
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log("Server Started !");
-        });
+            console.log("Server Started !"); 
+        }); 
     })
     .catch((error) => {
         console.log(error);
